b87fa1b7b4d0f24324fd3e70532234ae371f2909
Remco-Thijssen <remco.thijssen07@gmail.com>
2023-11-11 12:38:10 +0100

    added dynamic array and gitignore

--- dynamic-array/DynamicArray.php
+++ dynamic-array/DynamicArray.php
@@ -1,2 +1,60 @@
 <?php
 
+namespace Cases;
+
+use SplFixedArray;
+
+class DynamicArray
+{
+    private SplFixedArray $array;
+    private int $size = 0;
+
+    public function __construct(int $size = 1) {
+        $this->array = new SplFixedArray($size);
+    }
+
+    public function get(string|int $key): mixed
+    {
+        return $this->array[$key];
+    }
+
+    public function set(int $key, mixed $value): void
+    {
+        while (($key / $this->array->getSize()) >= 1) {
+            $this->resize();
+        }
+
+        $this->array[$key] = $value;
+
+        if ($key > $this->size) {
+            $this->size = ($key + 1);
+        }
+    }
+
+    public function add(mixed $value): void
+    {
+        if ($this->size == $this->array->getSize()) {
+            $this->resize();
+        }
+
+        $this->array[$this->size] = $value;
+        $this->size++;
+    }
+
+    public function getSize(): int
+    {
+        return $this->size;
+    }
+
+    private function resize(): void
+    {
+        $oldArray = $this->array;
+        $newArray = new SplFixedArray($this->array->getSize() * 2);
+
+        foreach ($oldArray as $key => $value) {
+            $newArray[$key] = $value;
+        }
+
+        $this->array = $newArray;
+    }
+}
--- dynamic-array/unit-tests/DynamicArrayTest.php
+++ dynamic-array/unit-tests/DynamicArrayTest.php
@@ -1,2 +1,97 @@
 <?php
 
+namespace UnitTests;
+
+include(__DIR__ . '/../DynamicArray.php');
+
+use Cases\DynamicArray;
+use Error;
+use OverflowException;
+use PHPUnit\Framework\TestCase;
+use RuntimeException;
+
+final class DynamicArrayTest extends TestCase
+{
+    public function testEmptyArray(): void
+    {
+        $dynamicArray = new DynamicArray();
+        $this->assertEquals(0, $dynamicArray->get(0));
+    }
+
+    public function testAddAndGetElement(): void
+    {
+        $dynamicArray = new DynamicArray();
+        $dynamicArray->add(5);
+        $this->assertEquals(5, $dynamicArray->get(0));
+    }
+
+    public function testResize(): void
+    {
+        $dynamicArray = new DynamicArray(2);
+        $dynamicArray->add(1);
+        $dynamicArray->add(2);
+        $dynamicArray->add(3);
+        $this->assertEquals(3, $dynamicArray->get(2));
+    }
+
+    public function testIndexOutOfRange(): void
+    {
+        $dynamicArray = new DynamicArray();
+        $this->expectException(RuntimeException::class);
+        $dynamicArray->get(1);
+    }
+
+    public function testEmptyArrayIndexZero(): void
+    {
+        $dynamicArray = new DynamicArray();
+        $this->assertEquals(null, $dynamicArray->get(0));
+    }
+
+    public function testIndexGreaterThanCurrentSize(): void
+    {
+        $dynamicArray = new DynamicArray();
+        $dynamicArray->set(10, 'test');
+        $this->assertEquals(null, $dynamicArray->get(1));
+        $this->assertEquals('test', $dynamicArray->get(10));
+    }
+
+    public function testSizeChangedAfterIndexSet(): void
+    {
+        $dynamicArray = new DynamicArray();
+        $dynamicArray->set(10, 'test');
+        $dynamicArray->set(4, 'test2');
+        $this->assertEquals(11, $dynamicArray->getSize());
+    }
+
+    public function testArraySize(): void
+    {
+        $dynamicArray = new DynamicArray();
+        $dynamicArray->add(5);
+        $dynamicArray->add(10);
+        $this->assertEquals(2, $dynamicArray->getSize());
+    }
+
+    public function testDifferentDataTypes(): void
+    {
+        $dynamicArray = new DynamicArray();
+        $dynamicArray->add(5);
+        $dynamicArray->add("Hello");
+        $this->assertEquals(5, $dynamicArray->get(0));
+        $this->assertEquals("Hello", $dynamicArray->get(1));
+    }
+
+    public function testLargeNumberOfElements(): void
+    {
+        $dynamicArray = new DynamicArray();
+        for ($i = 0; $i < 1000; $i++) {
+            $dynamicArray->add($i);
+        }
+        $this->assertEquals(999, $dynamicArray->get(999));
+    }
+
+    public function testNegativeSize(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $dynamicArray = new DynamicArray(-5);
+    }
+}
