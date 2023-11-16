508a9266484238b51e03850c9c6ec5c07f99e047
Roy van Empel <roy@wearekonzept.nl>
2023-11-16 15:00:14 +0100

    Update doublyLinkedList performance

--- cases/doubly-linked-list/DoublyLinkedList.php
+++ cases/doubly-linked-list/DoublyLinkedList.php
@@ -9,19 +9,24 @@ class DoublyLinkedList
     private ?Node $current = null;
     private int $size = 0;
 
+    public function __construct()
+    {
+        $this->head = new Node('HEAD');
+        $this->tail = new Node('TAIL');
+
+        $this->head->next = &$this->tail;
+        $this->tail->prev = &$this->head;
+    }
+
     public function append(mixed $data): void
     {
         $node = new Node($data);
 
-        if ($this->head === null) {
-            $this->head = &$node;
-            $this->tail = &$node;
-            $this->current = &$node;
-        } else {
-            $this->tail->next = &$node;
-            $node->prev = &$this->tail;
-            $this->tail = &$node;
-        }
+        $node->prev = &$this->tail->prev;
+        $node->next = &$this->tail;
+
+        $this->tail->prev->next = &$node;
+        $this->tail->prev = &$node;
 
         $this->size++;
     }
@@ -30,146 +35,157 @@ class DoublyLinkedList
     {
         $node = new Node($data);
 
-        $this->current = &$node;
+        $node->prev = &$this->head;
+        $node->next = &$this->head->next;
 
-        if ($this->head === null) {
-            $this->head = &$node;
-            $this->tail = &$node;
-        } else {
-            $this->head->prev = &$node;
-            $node->next = &$this->head;
-            $this->head = &$node;
-        }
+        $this->head->next->prev = &$node;
+        $this->head->next = &$node;
 
         $this->size++;
     }
 
-    public function insertBefore(mixed $searchData, mixed $newData): void
+    public function insertBefore(mixed $before, mixed $data): void
     {
-        $node = new Node($newData);
+        $node = new Node($data);
         
-        $foundNode = $this->find($searchData);
-        
-        $node->next = &$foundNode;
+        $foundNode = $this->find($before);
+
+        $foundNode->prev->next = &$node;
         $node->prev = &$foundNode->prev;
 
-        if ($foundNode->has('prev')) {
-            $foundNode->prev->next = &$node;
-        } else {
-            $this->head = &$node;
-        }
-
         $foundNode->prev = &$node;
+        $node->next = &$foundNode;
 
         $this->size++;
     }
 
-    public function insertAfter(mixed $searchData, mixed $newData): void
+    public function insertAfter(mixed $after, mixed $data): void
     {
-        $node = new Node($newData);
+        $node = new Node($data);
         
-        $foundNode = $this->find($searchData);
-        
-        $node->prev = &$foundNode;
+        $foundNode = $this->find($after);
+
+        $foundNode->next->prev = &$node;
         $node->next = &$foundNode->next;
 
-        if ($foundNode->has('next')) {
-            $foundNode->next->prev = &$node;
-        } else {
-            $this->head = &$node;
-        }
-
         $foundNode->next = &$node;
+        $node->prev = &$foundNode;
 
         $this->size++;
     }
 
-    public function get(mixed $value): mixed
+    public function shift(): mixed
     {
-        return $this->find($value)->data;
+        $data = $this->head->next->data;
+
+        $this->head->next = &$this->head->next->next;
+        $this->head->next->prev = &$this->head;
+
+        $this->size--;
+
+        return $data;
     }
 
-    public function remove(mixed $value)
+    public function pop(): mixed
+    {
+        $data = $this->tail->prev->data;
+
+        $this->tail->prev = &$this->tail->prev->prev;
+        $this->tail->prev->next = &$this->tail;
+
+        $this->size--;
+
+        return $data;
+    }
+
+    public function remove(mixed $value): void
     {
         $node = $this->find($value);
 
-        if ($this->current === $node) {
-            $this->current = &$this->head;
-        }
-
-        if ($node->has('prev')) {
-            $node->prev->next = &$node->next;
-        } else {
-            $this->head = &$node->next;
-        }
-
-        if ($node->has('next')) {
-            $node->next->prev = &$node->prev;
-        } else {
-            $this->tail = &$node->prev;
-        }
+        $node->prev->next = &$node->next;
+        $node->next->prev = &$node->prev;
 
         $this->size--;
     }
 
-    public function next(): mixed
-    {
-        $this->current = &$this->current->next;
+    // public function next(): mixed
+    // {
+    //     if ($this->current === null) {
+    //         $this->current = &$this->head->next;
+    //     }
 
-        if ($this->current === null) {
-            throw new \Exception('Index out of range');
-        }
 
-        return $this->current->data;
-    }
 
-    public function prev(): mixed
-    {
-        $this->current = &$this->current->prev;
+        // $this->current = &$this->current->next;
 
-        if ($this->current === null) {
-            throw new \Exception('Index out of range');
-        }
+        // if ($this->current === null) {
+        //     throw new \Exception('Index out of range');
+        // }
 
-        return $this->current->data;
-    }
+    //     return $this->current->data;
+    // }
+
+    // public function prev(): mixed
+    // {
+    //     $this->current = &$this->current->prev;
+
+    //     if ($this->current === null) {
+    //         throw new \Exception('Index out of range');
+    //     }
+
+    //     return $this->current->data;
+    // }
 
     public function getSize(): int
     {
         return $this->size;
     }
 
-    public function start(): ?Node
+    public function start(): mixed
     {
-        $this->current = &$this->head;
-        return $this->head;
+        $this->current = &$this->head->next;
+        return $this->current->data;
     }
 
-    public function end(): ?Node
+    public function end(): mixed
     {
-        $this->current = &$this->tail;
-        return $this->tail;
+        $this->current = &$this->tail->prev;
+        return $this->current->data;
     }
 
-    public function current(): ?Node
+    public function current(): mixed
     {
-        return $this->current;
+        return $this->current?->data;
     }
 
-    private function find(mixed $data): Node
+    public function find(mixed $data): ?Node
     {
-        $head = &$this->head;
-        for ($i = 0; $i < $this->getSize(); $i++) {
-            if ($data === $head->data) {
-                return $head;
+        $node = &$this->head->next;
+
+        while ($node !== null) {
+            if ($data === $node->data) {
+                return $node;
             }
-
-            $head = &$head->next;
-
-            if ($head === null) {
-                throw new \Exception('Index out of range');
-            }
+            $node = &$node->next;
         }
+
+        throw new \Exception('Data not found in list');
+    }
+
+    public function toArray(): array
+    {
+        $node = &$this->head->next;
+        $array = [];
+
+        while (isset($node->next)) {
+            $array[$node->data] = [
+                'prev' => $node->prev?->data,
+                'next' => $node->next?->data,
+            ];
+            $node = &$node->next;
+        }
+
+        return $array;
     }
 }
 
@@ -182,20 +198,5 @@ class Node
     public function __construct(mixed $data)
     {
         $this->data = $data;
-    }
-
-    public function has(string|array $type): bool
-    {
-        if (is_string($type)) {
-            return isset($this->$type);
-        }
-
-        foreach ($type as $value) {
-            if (!isset($this->$value)) {
-                return false;
-            }
-        }
-
-        return true;
     }
 }
