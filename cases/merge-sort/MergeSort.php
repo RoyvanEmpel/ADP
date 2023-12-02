ddccb1a7629495f9e969f326e125efd75942a3b9
Remco Thijssen <53993808+MasterInPvP@users.noreply.github.com>
2023-12-02 14:24:04 +0000

    mergesort fix

--- cases/merge-sort/MergeSort.php
+++ cases/merge-sort/MergeSort.php
@@ -49,69 +49,11 @@ class MergeSort
     }
 }
 
-class mergeSortMulti
-{
-    public static function mergeSort(array &$array)
-    {
-        $tempArray = [];
-        self::__mergeSort($array, $tempArray, 0, (count($array) - 1));
-    }
-    
-    private static function __mergeSort(array &$array, array &$tempArray, int $left, int $right)
-    {
-        if ($left < $right) {
-            $center = (int)(($left + $right) / 2);
-            
-            $pid = pcntl_fork();
-            if ($pid == -1) {
-                die('could not fork');
-            } else if ($pid) {
-                // we are the parent   
-                self::__mergeSort($array, $tempArray, ($center + 1), $right);
-                pcntl_waitpid($pid, $status); //Protect against Zombie children
-            } else {
-                // we are the child
-                self::__mergeSort($array, $tempArray, $left, $center);     
-                exit; // needed to prevent undesirable side-effects (explained below)
-            }
-            self::merge($array, $tempArray, $left, ($center + 1), $right);
-        }
-    }
-
-    private static function merge(array &$array, array &$tempArray, int $left, int $center, int $right)
-    {
-        for ($i = $left; $i <= $right; $i++) {
-            $tempArray[$i] = $array[$i];
-        }
-
-        $iterationLeft = $left;
-        $iterationRight = $center;
-        $current = $left;
-
-        while ($iterationLeft < $center && $iterationRight <= $right) {
-            if ($tempArray[$iterationLeft] <= $tempArray[$iterationRight]) {
-                $array[$current] = $tempArray[$iterationLeft];
-                $iterationLeft++;
-            } else {
-                $array[$current] = $tempArray[$iterationRight];
-                $iterationRight++;
-            }
-            $current++;
-        }
-
-        while ($iterationLeft < $center) {
-            $array[$current] = $tempArray[$iterationLeft];
-            $iterationLeft++;
-            $current++;
-        }
-    }
-}
-
 function testMergeSort(array $inputArray)
 {
     echo "Original Array: " . implode(" ", $inputArray) . PHP_EOL;
 
-    mergeSortMulti::mergeSort($inputArray);
+    mergeSort::mergeSort($inputArray);
 
     echo "Sorted Array: " . implode(" ", $inputArray) . PHP_EOL;
 }
