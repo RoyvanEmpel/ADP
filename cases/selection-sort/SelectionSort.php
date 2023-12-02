15ef613a6e03ea2d4243e2fc870181ac46e15d3e
Remco-Thijssen <remco.thijssen07@gmail.com>
2023-12-02 14:26:57 +0100

    Finished selection sort

--- cases/selection-sort/SelectionSort.php
+++ cases/selection-sort/SelectionSort.php
@@ -1,21 +1,23 @@
 <?php
 
 
-function selectionSort(array &$array)
+function selectionSort(array &$array, $order = 'ASC')
 {
     $arrayLength = count($array);
 
     for ($i = 0; $i < $arrayLength - 1; $i++) { 
-        $minIndex = $i;
+        $orderIndex = $i;
 
         for ($j = ($i + 1); $j < $arrayLength; $j++) { 
-            if ($array[$j] < $array[$minIndex]) {
-                $minIndex = $j;
+            if (strtolower($order) == 'asc' && $array[$j] < $array[$orderIndex]) {
+                $orderIndex = $j;
+            } else if (strtolower($order) == 'desc' && $array[$j] > $array[$orderIndex]) {
+                $orderIndex = $j;
             }
         }
 
-        $currentValue = $array[$minIndex];
-        $array[$minIndex] = $array[$i];
+        $currentValue = $array[$orderIndex];
+        $array[$orderIndex] = $array[$i];
         $array[$i] = $currentValue;
     }
 }
--- cases/selection-sort/unit-tests/SelectionSortTest.php
+++ cases/selection-sort/unit-tests/SelectionSortTest.php
@@ -8,12 +8,14 @@ use PHPUnit\Framework\TestCase;
 
 class SelectionSortTest extends TestCase
 {
-    private function isSorted(array $array): bool
+    private function isSorted(array $array, $order = 'asc'): bool
     {
         $length = count($array);
 
         for ($i = 0; $i < $length - 1; $i++) {
-            if ($array[$i] > $array[$i + 1]) {
+            if (strtolower($order) == 'asc' && $array[$i] > $array[$i + 1]) {
+                return false;
+            } else if (strtolower($order) == 'desc' && $array[$i] < $array[$i + 1]) {
                 return false;
             }
         }
@@ -21,6 +23,7 @@ class SelectionSortTest extends TestCase
         return true;
     }
 
+    // ASC - sorting
     public function testRandomArray()
     {
         $randomArray = [];
@@ -61,7 +64,6 @@ class SelectionSortTest extends TestCase
             $sortedArray[] = $i;
         }
         
-
         $originalArray = $sortedArray;
         $this->assertEquals($originalArray, $sortedArray, 'Arrays should be initially sorted.');
         $this->assertFalse($this->isSorted($sortedArray), 'Validate that the array is sorted');
@@ -88,6 +90,74 @@ class SelectionSortTest extends TestCase
         $this->assertEquals($originalArray, $singleNumArray, 'Check if after the method the array is still the same as before.');
     }
 
+
+    // Desc sorting
+    public function testRandomArrayDesc()
+    {
+        $randomArray = [];
+        for ($i = 0; $i < 100; $i++) { 
+            $randomArray[] = rand(0, 10000);
+        }
+
+        $originalArray = $randomArray;
+        $this->assertEquals($originalArray, $randomArray, 'Arrays should not be initially sorted.');
+        $this->assertFalse($this->isSorted($randomArray, 'desc'), 'Validate that the array is not sorted');
+
+        selectionSort($randomArray, 'desc');
+
+        $this->assertTrue($this->isSorted($randomArray, 'desc'), 'Array should be sorted after selection sort.');
+    }
+
+    public function testSortedArrayDesc()
+    {
+        $sortedArray = [];
+        for ($i = 0; $i < 100; $i++) { 
+            $sortedArray[] = $i;
+        }
+
+        $originalArray = $sortedArray;
+        $this->assertEquals($originalArray, $sortedArray, 'Arrays should be initially sorted.');
+        $this->assertFalse($this->isSorted($sortedArray, 'desc'), 'Validate that the array is sorted');
+
+        selectionSort($sortedArray, 'desc');
+
+        $this->assertTrue($this->isSorted($sortedArray, 'desc'), 'Array should be sorted after selection sort.');
+    }
+
+    public function testDescArrayDesc()
+    {
+        $sortedArray = [];
+        for ($i = 100; $i >= 0; $i--) { 
+            $sortedArray[] = $i;
+        }
+        
+        $originalArray = $sortedArray;
+        $this->assertEquals($originalArray, $sortedArray, 'Arrays should be initially sorted.');
+        $this->assertTrue($this->isSorted($sortedArray, 'desc'), 'Validate that the array is sorted');
+
+        selectionSort($sortedArray, 'desc');
+
+        $this->assertTrue($this->isSorted($sortedArray, 'desc'), 'Array should be sorted after selection sort.');
+    }
+
+    public function testSingleNumberArrayDesc()
+    {
+        $singleNumArray = [];
+        for ($i = 0; $i < 100; $i++) { 
+            $singleNumArray[] = 2;
+        }
+
+        $originalArray = $singleNumArray;
+        $this->assertEquals($originalArray, $singleNumArray, 'Array should be sorted, but this is for the insertion to check');
+        $this->assertTrue($this->isSorted($singleNumArray, 'desc'), 'Array should be sorted');
+
+        selectionSort($singleNumArray, 'desc');
+
+        $this->assertTrue($this->isSorted($singleNumArray, 'desc'), 'Array should be sorted after selection sort.');
+        $this->assertEquals($originalArray, $singleNumArray, 'Check if after the method the array is still the same as before.');
+    }
+
+    // JSON files
     public function testWithJSONData(): void
     {
         $jsonContents = file_get_contents(__DIR__ . '/../../../assets/json/dataset_sorteren.json');
@@ -96,6 +166,7 @@ class SelectionSortTest extends TestCase
         }
 
         if (is_object($jsonContents)) {
+            // ASC - sorting
             foreach ($jsonContents as $testData) {
                 $array = [];
                 foreach ($testData as $value) {
@@ -105,6 +176,18 @@ class SelectionSortTest extends TestCase
                 selectionSort($array);
                 
                 $this->assertTrue($this->isSorted($array), 'Check if its sorted after the selectionSort. This should always be true');
+            }
+
+            // DESC - sorting
+            foreach ($jsonContents as $testData) {
+                $array = [];
+                foreach ($testData as $value) {
+                    $array[] = $value;
+                }
+
+                selectionSort($array, 'desc');
+                
+                $this->assertTrue($this->isSorted($array, 'desc'), 'Check if its sorted after the selectionSort. This should always be true');
             }
         }
     }
