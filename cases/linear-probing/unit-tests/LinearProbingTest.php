fbce0a1b6dca2a07e79923b601ce9b3518888eba
Roy van Empel <roy@wearekonzept.nl>
2023-12-09 14:26:30 +0100

    Add LinearProbing Files

---
+++ cases/linear-probing/unit-tests/LinearProbingTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace UnitTests;
+
+include(__DIR__ . '/../LinearProbing.php');
+
+use PHPUnit\Framework\TestCase;
+use Cases\LinearProbing;
+
+class LinearProbingTest extends TestCase
+{
+}
