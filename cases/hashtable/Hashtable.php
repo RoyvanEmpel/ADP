bf185f83a875eddb253815f9e817d1c0c8f4fbfd
Roy van Empel <roy@wearekonzept.nl>
2023-12-09 14:41:12 +0100

    Update hashtable name

---
+++ cases/hashtable/Hashtable.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Cases;
+
+class Hashtable
+{
+
+  public function insert(string $key, mixed $value): void
+  {
+
+  }
+
+  public function get(string $key): mixed
+  {
+
+  }
+
+  public function delete(string $key): void
+  {
+
+  }
+
+  public function update(string $key, mixed $nextValue): void
+  {
+    
+  }
+
+
+}
--- cases/linear-probing/LinearProbing.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Cases;
-
-class LinearProbing
-{
-
-}
